336f0248b7b40ea1175b59a7d775944268eba764
Daine Trinidad <daine.trinidad@gmail.com>
2011-07-21 15:45:38 -0700

    Added license

--- eniad/logger.php
+++ eniad/logger.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * 
+ * This is the logger
+ * @author Daine Trinidad
+ * 
+ * Copyright 2011 Daine Trinidad
+ * 
+ * This file is part of Eniad Application Framework.
+ * 
+ * Eniad Application Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Eniad Application Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Eniad Application Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
 class Logger{
 	public $filename;
 	public $folder;
